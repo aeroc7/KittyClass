45de99250b398dbe8c3faf0f67064c81c2f44012
bennett <aerozotac@outlook.com>
2021-12-17 15:35:21 -0800

    Add website backend to recieve frontend data bound for nn_server

---
+++ web_backend/upload.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const zmq = require("zeromq")
+
+async function forwardData(data) {
+    const sock = new zmq.Request
+
+    sock.connect("tcp://localhost:7373");
+    
+    console.log("Sending data to NN");
+    console.log(data.length)
+    await sock.send(data);
+    const [result] = await sock.receive();
+    console.log(result + '\n');
+}
+
+function onRequest(req, res) {
+    let data = '';
+    req.on('data', chunk => {
+        data += chunk;
+    });
+
+    req.on('end', () => {
+        console.log("Recieved data");
+        forwardData(data)
+    });
+}
+
+http.createServer(onRequest).listen(5000);
+console.log("Server is running");
