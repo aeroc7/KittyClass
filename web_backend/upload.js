ca548c3e654c82547658392e9edee8a4ea5f0ec2
Bennett Anderson <aerozotac@outlook.com>
2021-12-18 16:05:06 -0800

    Only send raw image over coket (remove base64 header that js' FileReader adds)

--- web_backend/upload.js
+++ web_backend/upload.js
@@ -8,6 +8,10 @@ async function forwardData(data) {
     
     console.log("Sending data to NN");
     console.log(data.length)
+    
+    // Remove base64 header, so we just have the raw data
+    data = data.split(',')[1];
+    
     await sock.send(data);
     const [result] = await sock.receive();
     console.log(result + '\n');
