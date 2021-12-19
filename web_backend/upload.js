7e5b4f29079d18adc8c2cc48167014eb1706e907
Bennett Anderson <aerozotac@outlook.com>
2021-12-18 23:17:49 -0800

    Finalize backend code

--- web_backend/upload.js
+++ web_backend/upload.js
@@ -1,33 +1,38 @@
 const http = require('http');
 const zmq = require("zeromq")
+require('dotenv').config()
 
 async function forwardData(data) {
     const sock = new zmq.Request
 
-    sock.connect("tcp://localhost:7373");
-    
-    console.log("Sending data to NN");
-    console.log(data.length)
-    
+    sock.connect("tcp://10.0.0.161:7373");
     // Remove base64 header, so we just have the raw data
     data = data.split(',')[1];
-    
+
     await sock.send(data);
-    const [result] = await sock.receive();
-    console.log(result + '\n');
+    return await sock.receive();
 }
 
-function onRequest(req, res) {
-    let data = '';
-    req.on('data', chunk => {
-        data += chunk;
-    });
+async function onRequest(req, res) {
+    const buffers = [];
 
-    req.on('end', () => {
-        console.log("Recieved data");
-        forwardData(data)
-    });
+    for await (const chunk of req) {
+        buffers.push(chunk);
+    }
+
+    const data = Buffer.concat(buffers).toString();
+
+    result = forwardData(data);
+    result
+        .then(result_data => {
+            res.writeHead(200, {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'text/plain'
+            });
+            res.write(result_data.toString());
+            res.end();
+        });
 }
 
-http.createServer(onRequest).listen(5000);
+http.createServer(onRequest).listen(5000, '10.0.0.161');
 console.log("Server is running");
